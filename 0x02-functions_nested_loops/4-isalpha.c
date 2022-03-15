83bf96132e451cf77ada8b7b1d95378f3a64fffb
4944Virginia <ocholavirginia@gmail>
2022-03-15 14:34:37 -0700

    lol

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,5 @@
 #include "main.h"
-/**
- * print_sign - prints the sign of a number
+ /* print_sign - prints the sign of a number
  * @n: takes in an integer
  * Return: 1 if n is > zero, 0 if n is zero, -1 if n is < zero
  */
