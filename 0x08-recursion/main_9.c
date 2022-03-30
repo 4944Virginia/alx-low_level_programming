41e114818050094cff38d46d96015afc1bc90561
4944Virginia <ocholavirginia@gmail>
2022-03-30 14:59:22 -0700

    compares two strings

--- 0x08-recursion/main_9.c
+++ 0x08-recursion/main_9.c
@@ -1,17 +1,9 @@
-#include <stdio.h>
-
-int wildcmp(char *, char *);
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int r;
-
-	r = wildcmp("school-school.c", "sch*ool.c");
-	printf("%d\n", r);
-	return (0);
-}
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
